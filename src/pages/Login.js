5cdad8f1a8b175fc931fcfb4cb046af5c91611a4
Shivam Shingare <shivam.s2818@gmail.com>
2025-09-04 12:00:25 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -46,7 +46,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/api/login", {
+      const res = await fetch("https://gorail-project.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: form.email.trim(), password: form.password }),
