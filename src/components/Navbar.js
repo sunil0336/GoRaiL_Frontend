c9f28519fd18a8937edffa500dea9f0b3e909938
Shivam Shingare <shivam.s2818@gmail.com>
2025-09-20 11:48:02 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,19 +21,28 @@ export default function Navbar() {
   return (
     <AppBar position="fixed" color="primary" sx={{ boxShadow: "none", fontWeight: "bold" }}>
       <Toolbar>
-        <Typography variant="h5" sx={{ flexGrow: 1 }}>GoRaiL</Typography>
+        <Typography variant="h5" sx={{ flexGrow: 1 }}>
+          GoRaiL
+        </Typography>
 
-        <Button color="inherit" component={Link} to="/" sx={{ textTransform: "none" }}>Home</Button>
-        <Button color="inherit" component={Link} to="/login" sx={{ textTransform: "none" }}>Login</Button>
-        <Button color="inherit" component={Link} to="/register" sx={{ textTransform: "none" }}>Register</Button>
-        <Button color="inherit" component={Link} to="/search" sx={{ textTransform: "none" }}>Search Trains</Button>
-
-        // {user && (
-        //   <>
-            <Button color="inherit" component={Link} to="/bookings" sx={{ textTransform: "none" }}>Bookings</Button>
-            <Button color="inherit" component={Link} to="/profile" sx={{ textTransform: "none" }}>Profile</Button>
-          // </>
-        )}
+        <Button color="inherit" component={Link} to="/" sx={{ textTransform: "none" }}>
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/login" sx={{ textTransform: "none" }}>
+          Login
+        </Button>
+        <Button color="inherit" component={Link} to="/register" sx={{ textTransform: "none" }}>
+          Register
+        </Button>
+        <Button color="inherit" component={Link} to="/search" sx={{ textTransform: "none" }}>
+          Search Trains
+        </Button>
+        <Button color="inherit" component={Link} to="/bookings" sx={{ textTransform: "none" }}>
+          Bookings
+        </Button>
+        <Button color="inherit" component={Link} to="/profile" sx={{ textTransform: "none" }}>
+          Profile
+        </Button>
       </Toolbar>
     </AppBar>
   );
